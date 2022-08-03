127b9b5d540f023f8e70c68e60159593adcfa064
ArgoNavis1763 <92317902+ArgoNavis1763@users.noreply.github.com>
2022-08-03 20:03:08 +0100

    Styling (#2)
    * button recording audio
    
    * play button plays recorded audio
    
    * disable play button while audio playing
    
    * records and plays
    
    * record button now returns artist album and title
    
    * env variable for node api url
    
    * refactoring
    
    * yarn lock removed
    
    * remove play button
    
    * tailwind added and background set
    
    * basic styling added and song card component created
    
    * first cypress tests
    
    * cypress mock done
    
    * first cypress tests working
    
    Co-authored-by: Charlotte Walker <charlielizzy@me.com>

--- cypress/component/RecordingButton.cy.js
+++
@@ -1,5 +0,0 @@
-describe('RecordingButton.cy.js', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
-})
---
+++ src/Components/SongCard/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+export const SongCard = (props) => {
+ 
+    return (
+     <div data-automation="songCard" className="tracking-widest m-5 p-3 bg-gray-900 rounded-lg w-60 text-3xl text-white">
+     <p>Title: {props.title} </p>
+     <br />
+     <p>Artist: {props.artist}</p>
+     <br />
+     <p>Album: {props.album}</p>
+     </div>
+     
+    )
+}
